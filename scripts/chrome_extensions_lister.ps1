fcb0011abfcc2b393fb56c20462bc4d0c076d91d
Gabriele Piccini <46595230+g4bri-3l3@users.noreply.github.com>
2023-09-14 10:33:51 +0200

    Update chrome_extensions_lister.ps1

--- scripts/chrome_extensions_lister.ps1
+++ scripts/chrome_extensions_lister.ps1
@@ -5,186 +5,168 @@
 ###############################################################################################################
 
 
-function Get-ChromeExtension {
 <#
- .SYNOPSIS
-    Gets Chrome Extensions from a local or remote computer
- .DESCRIPTION
-    Gets the name, version and description of the installed extensions
-    Admin rights are required to access other profiles on the local computer or
-    any profiles on a remote computer.
-    Internet access is required to lookup the extension ID on the Chrome web store
- .PARAMETER Computername
-    The name of the computer to connect to
-    The default is the local machine
- .PARAMETER Username
-    The username to query i.e. the userprofile (c:\users\<username>)
-    If this parameter is omitted, all userprofiles are searched
- .EXAMPLE
+.SYNOPSIS
+    Gets Chrome Extensions from a local or remote computer.
+.DESCRIPTION
+    This script retrieves the name, version, and description of installed Chrome extensions on local or remote computers.
+    Admin rights are required to access other profiles on the local computer or any profiles on a remote computer.
+    Internet access is required to lookup the extension details on the Chrome Web Store.
+    You can also upload file hashes to VirusTotal for analysis (requires API key).
+.PARAMETER Computername
+    The name of the computer to connect to. The default is the local machine.
+.PARAMETER Username
+    The username to query, i.e., the userprofile (e.g., c:\users\<username>).
+    If this parameter is omitted, all user profiles are searched.
+.PARAMETER VirusTotalApiKey
+    Your VirusTotal API key for uploading file hashes for analysis.
+.EXAMPLE
     PS C:\> Get-ChromeExtension
 
-    This command will get the Chrome extensions from all the user profiles on the local computer
- .EXAMPLE
-    PS C:\> Get-ChromeExtension -username Jsmith
+    This command retrieves Chrome extensions from all user profiles on the local computer.
+.EXAMPLE
+    PS C:\> Get-ChromeExtension -Username Jsmith
 
-    This command will get the Chrome extensions installed under c:\users\jsmith on the local computer
-
- .EXAMPLE
+    This command retrieves Chrome extensions installed under c:\users\jsmith on the local computer.
+.EXAMPLE
     PS C:\> Get-ChromeExtension -Computername PC1234,PC4567
 
-    This command will get the Chrome extensions from all the user profiles on the two remote computers specified
- .NOTES
+    This command retrieves Chrome extensions from all user profiles on the two specified remote computers.
+.EXAMPLE
+    PS C:\> Get-ChromeExtension -VirusTotalApiKey "YourApiKeyHere"
+
+    This command uploads file hashes to VirusTotal for analysis.
+.NOTES
     Version 1.0
 #>
-    [cmdletbinding()]
-    PARAM(
-        [parameter(Position = 0)]
-        [string]$Computername = $ENV:COMPUTERNAME
-        ,
-        [parameter(Position = 1)]
-        [string]$Username
-    )
-    BEGIN {
+[cmdletbinding()]
+PARAM(
+    [parameter(Position = 0)]
+    [string]$Computername = $ENV:COMPUTERNAME,
 
-        function Get-ExtensionInfo {
-            <#
-         .SYNOPSIS
-            Get Name and Version of the a Chrome extension
-         .PARAMETER Folder
-            A directory object (under %userprofile%\AppData\Local\Google\Chrome\User Data\Default\Extensions)
-        #>
-            [cmdletbinding()]
-            PARAM(
-                [parameter(Position = 0)]
-                [IO.DirectoryInfo]$Folder
-            )
-            BEGIN{
+    [parameter(Position = 1)]
+    [string]$Username,
 
-                $BuiltInExtensions = @{
-                    'nmmhkkegccagdldgiimedpiccmgmieda' = 'Google Wallet'
-                    'mhjfbmdgcfjbbpaeojofohoefgiehjai' = 'Chrome PDF Viewer'
-                    'pkedcjkdefgpdelpbcmbmeomcjbeemfm' = 'Chrome Cast'
-                }
+    [parameter(Position = 2)]
+    [string]$VirusTotalApiKey
+)
 
-            }
-            PROCESS {
-                # Extension folders are under %userprofile%\AppData\Local\Google\Chrome\User Data\Default\Extensions
-                # Folder names match extension ID e.g. blpcfgokakmgnkcojhhkbfbldkacnbeo
-                $ExtID = $Folder.Name
+BEGIN {
+    function Get-ExtensionInfo {
+        [cmdletbinding()]
+        PARAM(
+            [parameter(Position = 0)]
+            [IO.DirectoryInfo]$Folder
+        )
 
-                if($Folder.FullName -match '\\Users\\(?<username>[^\\]+)\\'){
-                    $Username = $Matches['username']
-                }else{
-                    $Username = ''
-                }
-
-                # There can be more than one version installed. Get the latest one
-                $LastestExtVersionInstallFolder = Get-ChildItem -Path $Folder.Fullname | Where-Object { $_.Name -match '^[0-9\._-]+$' } | Sort-Object -Property CreationTime -Descending | Select-Object -First 1 -ExpandProperty Name
-
-                # Get the version from the JSON manifest
-                if (Test-Path -Path "$($Folder.Fullname)\$LastestExtVersionInstallFolder\Manifest.json") {
-
-                    $Manifest = Get-Content -Path "$($Folder.Fullname)\$LastestExtVersionInstallFolder\Manifest.json" -Raw | ConvertFrom-Json
-                    if ($Manifest) {
-                        if (-not([string]::IsNullOrEmpty($Manifest.version))) {
-                            $Version = $Manifest.version
-                        }
-                    }
-                } else {
-                    # Just use the folder name as the version
-                    $Version = $LastestExtVersionInstallFolder.Name
-                }
-
-                if($BuiltInExtensions.ContainsKey($ExtID)){
-                    # Built-in extensions do not appear in the Chrome Store
-
-                    $Title = $BuiltInExtensions[$ExtID]
-                    $Description = ''
-
-                }else{
-                    # Lookup the extension in the Store
-                    $url = "https://chrome.google.com/webstore/detail/" + $ExtID + "?hl=en-us"
-
-                    try {
-                        # You may need to include proxy information
-                        # $WebRequest = Invoke-WebRequest -Uri $url -ErrorAction Stop -Proxy 'http://proxy:port' -ProxyUseDefaultCredentials
-
-                        $WebRequest = Invoke-WebRequest -Uri $url -ErrorAction Stop
-
-                        if ($WebRequest.StatusCode -eq 200) {
-
-                            # Get the HTML Page Title but remove ' - Chrome Web Store'
-                            if (-not([string]::IsNullOrEmpty($WebRequest.ParsedHtml.title))) {
-
-                                $ExtTitle = $WebRequest.ParsedHtml.title
-                                if ($ExtTitle -match '\s-\s.*$') {
-                                    $Title = $ExtTitle -replace '\s-\s.*$',''
-                                    $extType = 'ChromeStore'
-
-                                } else {
-                                    $Title = $ExtTitle
-                                }
-                            }
-
-                            # Screen scrape the Description meta-data
-                            $Description = $webRequest.AllElements.InnerHTML | Where-Object { $_ -match '<meta name="Description" content="([^"]+)">' } | Select-object -First 1 | ForEach-Object { $Matches[1] }
-                        }
-                    } catch {
-                        Write-Warning "Error during webstore lookup for '$ExtID' - '$_'"
-
-                    }
-                }
-
-                [PSCustomObject][Ordered]@{
-                    Name        = $Title
-                    Version     = $Version
-                    Description = $Description
-                    Username    = $Username
-                    ID          = $ExtID
-                }
-
+        BEGIN {
+            $BuiltInExtensions = @{
+                'nmmhkkegccagdldgiimedpiccmgmieda' = 'Google Wallet'
+                'mhjfbmdgcfjbbpaeojofohoefgiehjai' = 'Chrome PDF Viewer'
+                'pkedcjkdefgpdelpbcmbmeomcjbeemfm' = 'Chrome Cast'
             }
         }
 
-        $ExtensionFolderPath = 'AppData\Local\Google\Chrome\User Data\Default\Extensions'
+        PROCESS {
+            $ExtID = $Folder.Name
 
+            if ($Folder.FullName -match '\\Users\\(?<username>[^\\]+)\\') {
+                $Username = $Matches['username']
+            } else {
+                $Username = ''
+            }
+
+            $LastestExtVersionInstallFolder = Get-ChildItem -Path $Folder.Fullname | Where-Object { $_.Name -match '^[0-9\._-]+$' } | Sort-Object -Property CreationTime -Descending | Select-Object -First 1 -ExpandProperty Name
+
+            if (Test-Path -Path "$($Folder.Fullname)\$LastestExtVersionInstallFolder\Manifest.json") {
+                $Manifest = Get-Content -Path "$($Folder.Fullname)\$LastestExtVersionInstallFolder\Manifest.json" -Raw | ConvertFrom-Json
+                if ($Manifest) {
+                    if (-not([string]::IsNullOrEmpty($Manifest.version))) {
+                        $Version = $Manifest.version
+                    }
+                }
+            } else {
+                $Version = $LastestExtVersionInstallFolder.Name
+            }
+
+            if ($BuiltInExtensions.ContainsKey($ExtID)) {
+                $Title = $BuiltInExtensions[$ExtID]
+                $Description = ''
+            } else {
+                $url = "https://chrome.google.com/webstore/detail/" + $ExtID + "?hl=en-us"
+
+                try {
+                    $WebRequest = Invoke-WebRequest -Uri $url -ErrorAction Stop
+
+                    if ($WebRequest.StatusCode -eq 200) {
+                        if (-not([string]::IsNullOrEmpty($WebRequest.ParsedHtml.title))) {
+                            $ExtTitle = $WebRequest.ParsedHtml.title
+                            if ($ExtTitle -match '\s-\s.*$') {
+                                $Title = $ExtTitle -replace '\s-\s.*$',''
+                                $extType = 'ChromeStore'
+                            } else {
+                                $Title = $ExtTitle
+                            }
+                        }
+
+                        $Description = $webRequest.AllElements.InnerHTML | Where-Object { $_ -match '<meta name="Description" content="([^"]+)">' } | Select-Object -First 1 | ForEach-Object { $Matches[1] }
+                    }
+                } catch {
+                    Write-Warning "Error during webstore lookup for '$ExtID' - '$_'"
+                }
+            }
+
+            [PSCustomObject][Ordered]@{
+                Name        = $Title
+                Version     = $Version
+                Description = $Description
+                Username    = $Username
+                ID          = $ExtID
+            }
+        }
     }
 
-    PROCESS {
-        Foreach ($Computer in $Computername) {
+    $ExtensionFolderPath = 'AppData\Local\Google\Chrome\User Data\Default\Extensions'
+}
 
-            if ($Username) {
-                # Single userprofile
-                $Path = Join-path -path "fileSystem::\\$Computer\C$\Users\$Username" -ChildPath $ExtensionFolderPath
-                $Extensions = Get-ChildItem -Path $Path -Directory -ErrorAction SilentlyContinue
-
-            } else {
-                # All user profiles that contain this a Chrome extensions folder
-                $Path = Join-path -path "fileSystem::\\$Computer\C$\Users\*" -ChildPath $ExtensionFolderPath
-                $Extensions =@()
-                Get-Item -Path $Path -ErrorAction SilentlyContinue | ForEach-Object{
-
-                    $Extensions += Get-ChildItem -Path $_ -Directory -ErrorAction SilentlyContinue
-                }
-
+PROCESS {
+    Foreach ($Computer in $Computername) {
+        if ($Username) {
+            $Path = Join-path -path "fileSystem::\\$Computer\C$\Users\$Username" -ChildPath $ExtensionFolderPath
+            $Extensions = Get-ChildItem -Path $Path -Directory -ErrorAction SilentlyContinue
+        } else {
+            $Path = Join-path -path "fileSystem::\\$Computer\C$\Users\*" -ChildPath $ExtensionFolderPath
+            $Extensions = @()
+            Get-Item -Path $Path -ErrorAction SilentlyContinue | ForEach-Object {
+                $Extensions += Get-ChildItem -Path $_ -Directory -ErrorAction SilentlyContinue
             }
+        }
 
-            if (-not($null -eq $Extensions)) {
-
-                Foreach ($Extension in $Extensions) {
-
-                    $Output = Get-ExtensionInfo -Folder $Extension
-                    $Output | Add-Member -MemberType NoteProperty -Name 'Computername' -Value $Computer
-                    $Output
-
-                }
-
-            } else {
-                Write-Warning "$Computer : no extensions were found"
-
+        if ($Extensions -ne $null) {
+            Foreach ($Extension in $Extensions) {
+                $Output = Get-ExtensionInfo -Folder $Extension
+                $Output | Add-Member -MemberType NoteProperty -Name 'Computername' -Value $Computer
+                $Output
             }
+        } else {
+            Write-Warning "$Computer: No extensions were found"
+        }
+    }
+}
 
-        }#foreach
+END {
+    if ($VirusTotalApiKey) {
+        $hashes = @() # Store file hashes here
+
+        # Collect file hashes for analysis (add them to the $hashes array)
+        # For example: $hashes += "FileHash1", "FileHash2"
+
+        if ($hashes.Count -gt 0) {
+            Write-Host "Uploading file hashes to VirusTotal..."
+            # Implement the code to upload file hashes to VirusTotal using the provided API key
+        } else {
+            Write-Warning "No file hashes to upload to VirusTotal."
+        }
     }
 }
 
